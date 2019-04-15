92dc0758d7d118434486e1360869f967fc2ee9df
Dave Kludt <oldarmyc@gmail.com>
2019-04-15 10:27:08 -0500

    Add test for dir check func

--- tests/tests_system_gather.py
+++ tests/tests_system_gather.py
@@ -688,4 +688,22 @@ class TestSystemProfile(TestCase):
 
     # Directory checks
     def test_directory_paths(self):
-        pass
+        expected_output = [
+            '/etc/chef',
+            '/etc/salt'
+        ]
+        mock_response = mock.Mock()
+        mock_response.side_effect = [
+            True, True, False, False, False, False, False, False, False
+        ]
+        with mock.patch(
+            'system_profile.profile.os.path.exists',
+            side_effect=mock_response
+        ):
+            returns = profile.check_dir_paths(True)
+
+        self.assertEquals(
+            expected_output,
+            returns,
+            'Returned values did not match expected output'
+        )
